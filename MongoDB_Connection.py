1860af9c142ecc58696089b04e1c3b3cbb0d480d
rguerra <rguerr@www-1>
2021-06-30 22:16:00 +0000

    Adding MongoDB_Connection.py

--- MongoDB_Connection.py
+++ MongoDB_Connection.py
@@ -18,7 +18,7 @@ import sys
 
 from pymongo import MongoClient
 from pprint import pprint
-client = MongoClient('127.0.0.1', 27017)
+client = MongoClient('data-mongodb', 27017)
 db = client.admin
 serverStatusResult=db.command("serverStatus")
 pprint(serverStatusResult)
